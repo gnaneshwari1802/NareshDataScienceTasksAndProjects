5ecd924bbb18b1f68dccf796f2f20946236653f0
gnaneshwari1802 <61177529+gnaneshwari1802@users.noreply.github.com>
2023-09-05 21:34:16 +0530

    Update housind price prediction by using MLR.py

--- housind price prediction by using MLR.py
+++ housind price prediction by using MLR.py
@@ -11,7 +11,13 @@ import seaborn as sns
 
 import warnings
 warnings.filterwarnings('ignore')
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
+#from subprocess import check_output
+#print(check_output(["ls", "../input"]).decode("utf8"))
+
+# Any results you write to the current directory are saved as output.
 
 # Import the data set
 
