4461538538103c41ec0b30cbc524faa45c2c2927
gnaneshwari1802 <61177529+gnaneshwari1802@users.noreply.github.com>
2023-09-05 22:40:31 +0530

    Update SLR Practicle.py

--- SLR Practicle.py
+++ SLR Practicle.py
@@ -52,6 +52,31 @@ pred = regressor.predict(xtest)
 
 # Visualizing the training Test Results
 plt.scatter(xtrain, ytrain, color='red')
+"""
+matplotlib.pyplot.scatter()
+Scatter plots are used to observe relationship between variables and uses dots to represent the relationship between them. The scatter() method in the matplotlib library is used to draw a scatter plot. Scatter plots are widely used to represent relation among variables and how change in one affects the other. 
+Syntax 
+The syntax for scatter() method is given below:
+ 
+
+matplotlib.pyplot.scatter(x_axis_data, y_axis_data, s=None, c=None, marker=None, cmap=None, vmin=None, vmax=None, alpha=None, linewidths=None, edgecolors=None) 
+ 
+
+The scatter() method takes in the following parameters: 
+ 
+
+
+x_axis_data- An array containing x-axis data
+y_axis_data- An array containing y-axis data
+s- marker size (can be scalar or array of size equal to size of x or y)
+c- color of sequence of colors for markers
+marker- marker style
+cmap- cmap name
+linewidths- width of marker border
+edgecolor- marker border color
+alpha- blending value, between 0 (transparent) and 1 (opaque)
+Except x_axis_data and y_axis_data all other parameters are optional and their default value is None. Below are the scatter plot examples with various parameters.
+"""
 plt.plot(xtrain, regressor.predict(xtrain), color='blue')
 plt.title("Visuals for Training Dataset")
 plt.xlabel("Space")
